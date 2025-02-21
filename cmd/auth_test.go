7ad8672d6a46e8da892b3914f6e45a572c28c301
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2025-02-21 09:48:59 +0300

    Update auth_test.go

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,6 +4,7 @@ import (
 	"LinkShorty/internal/auth"
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -23,5 +24,34 @@ func TestLoginSuccess(t *testing.T) {
 	}
 	if res.StatusCode != 200 {
 		t.Fatalf("Expected 200 got %d", res.StatusCode)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resp auth.LoginResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.Token == "" {
+		t.Fatalf("Expected non-empty token")
+	}
+}
+
+func TestLoginFail(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "test@example.com",
+		Password: "passwordFail",
+	})
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 401 {
+		t.Fatalf("Expected 401 got %d", res.StatusCode)
 	}
 }
